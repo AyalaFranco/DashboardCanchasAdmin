f3dcf63da95f7184452aa7727811f0fa872bd0ed
angelvicentecruz2006 <angelvicentecruz2006@gmail.com>
2026-04-21 01:34:27 -0300

    imagenes canchas

--- lib/model/cancha.dart
+++ lib/model/cancha.dart
@@ -4,6 +4,8 @@ class Cancha {
   final String ubicacion;
   final String telefono;
   final String tipoCancha;
+  final String imagenCancha;
+  final String logoCancha;
   final List<String> servicios;
 
   const Cancha({
@@ -12,6 +14,8 @@ class Cancha {
     required this.ubicacion,
     required this.telefono,
     required this.tipoCancha,
+    required this.imagenCancha,
+    required this.logoCancha,
     required this.servicios,
   });
 
@@ -22,6 +26,8 @@ class Cancha {
       ubicacion: "Fleming 540",
       telefono: "2945331590",
       tipoCancha: "Césped Sintético",
+      imagenCancha: "Fleming.jpeg",
+      logoCancha: "fleming.png",
       servicios: [
         "wi-fi, torneos, estacionamiento, parilla, cumpleaños, bar, quincho",
       ],
@@ -32,16 +38,20 @@ class Cancha {
       ubicacion: "Av. Fontana 835",
       telefono: "(02945) 15681194",
       tipoCancha: "Futbol Sala",
+      imagenCancha: "Club San Martín.png",
+      logoCancha: "san martin.jpg",
       servicios: [
         "wi-fi, torneos, estacionamiento, parilla, cumpleaños, bar, quincho",
       ],
     ),
     Cancha(
       id: 2,
-      nombre: "Club Social y Deportivo Del Campo",
+      nombre: "La Española",
       ubicacion: "Rivadavia y Mitre",
       telefono: "(02945) 15334303",
       tipoCancha: "Cesped Natural",
+      imagenCancha: "La Española.jpg",
+      logoCancha: "española.png",
       servicios: [
         "wi-fi, torneos, estacionamiento, parilla, cumpleaños, bar, quincho",
       ],
@@ -52,6 +62,8 @@ class Cancha {
       ubicacion: "Alberdi 251",
       telefono: "(02945) 15553978",
       tipoCancha: "Césped Sintético",
+      imagenCancha: "El Molino.png",
+      logoCancha: "",
       servicios: [
         "wi-fi, torneos, estacionamiento, parilla, cumpleaños, bar, quincho",
       ],
@@ -62,6 +74,8 @@ class Cancha {
       ubicacion: "Chacabuco 1791",
       telefono: "2945594466",
       tipoCancha: "Césped Sintético",
+      imagenCancha: "Esquel Padding.png",
+      logoCancha: "padel.png",
       servicios: [
         "wi-fi, torneos, estacionamiento, parilla, cumpleaños, bar, quincho",
       ],
@@ -72,6 +86,8 @@ class Cancha {
       ubicacion: "Ruta 259 Km. 4",
       telefono: "(02945) 15667025",
       tipoCancha: "Césped Sintético, Cancha Techada",
+      imagenCancha: "La Gran 5.png",
+      logoCancha: "gran 5.png",
       servicios: [
         "wi-fi, torneos, estacionamiento, parilla, cumpleaños, bar, quincho",
       ],
@@ -82,6 +98,8 @@ class Cancha {
       ubicacion: "Brun 750",
       telefono: "(02945) 15403268",
       tipoCancha: "Césped Sintético",
+      imagenCancha: "La 10 Fc.png",
+      logoCancha: "la 10.png",
       servicios: [
         "wi-fi, torneos, estacionamiento, parilla, cumpleaños, bar, quincho",
       ],
@@ -92,26 +110,20 @@ class Cancha {
       ubicacion: "Conesa 986",
       telefono: "(02945) 15339230",
       tipoCancha: "Futbol Sala",
+      imagenCancha: "La Gambeta.png",
+      logoCancha: "gambeta.png",
       servicios: [
         "wi-fi, torneos, estacionamiento, parilla, cumpleaños, bar, quincho",
       ],
     ),
     Cancha(
       id: 8,
-      nombre: "La Martita Fútbol de Campo",
-      ubicacion: "Ruta 259 Km. 4",
-      telefono: "2945416644",
-      tipoCancha: "Césped Sintético",
-      servicios: [
-        "wi-fi, torneos, estacionamiento, parilla, cumpleaños, bar, quincho",
-      ],
-    ),
-    Cancha(
-      id: 9,
       nombre: "Zona Mixta",
       ubicacion: "Almafuerte 1191",
       telefono: "2213548408",
       tipoCancha: "Césped Sintético",
+      imagenCancha: "Zona Mixta.jpg",
+      logoCancha: "zona mixta.png",
       servicios: [
         "wi-fi, torneos, estacionamiento, parilla, cumpleaños, bar, quincho",
       ],
