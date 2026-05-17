04a3b24ef7f4288db9f271a5b0033aadc283cc6d
angelobottallo <angelobottallo@gmail.com>
2026-05-17 18:38:34 -0300

    Autenticacion biometrica (falta corregir flujo de loginscreen)

--- android/app/src/main/kotlin/com/example/reserva_cancha/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/reserva_cancha/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.reserva_cancha
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,7 +1,42 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:local_auth/local_auth.dart';
+
+
 
 class AuthService { //Servicios de autenticacion (Inicio de sesion/registro/cierre)
   final SupabaseClient _client = Supabase.instance.client; //Aceder al cliente
+  final LocalAuthentication auth = LocalAuthentication();
+
+Future<bool> authenticate() async {
+  // Verificar si el dispositivo soporta biometría
+  final bool canCheck = await auth.canCheckBiometrics;
+  final bool isSupported = await auth.isDeviceSupported();
+
+  if (!canCheck || !isSupported) return false;
+
+  try {
+    final bool didAuthenticate = await auth.authenticate(
+      localizedReason: 'Autenticate para continuar',
+      options: const AuthenticationOptions(
+        biometricOnly: false,  // false = permite PIN como fallback
+        stickyAuth: true,     // mantiene el diálogo si la app pasa a background
+      ),
+    );
+
+    if (didAuthenticate) {
+      return true;
+    }
+  } on Exception catch (e) {
+    
+    print('Error: ${ e.toString()}');
+   
+  }
+  return false;
+}
+
+
+
+
 
   Future<void> signUp(String email, String password)async { //Metodo signUp
     final response = await _client.auth.signUp( 
